ca0613eab719f31163300f381b55919cadda865c
H S <gkskfhdlstm@naver.com>
2019-05-30 02:19:12 +0900

    SlotAnimation효과적용

--- app/src/main/java/com/onsemiro/hanpinetree/koreatechrandomfood/MainActivity.java
+++ app/src/main/java/com/onsemiro/hanpinetree/koreatechrandomfood/MainActivity.java
@@ -4,13 +4,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private ImageView mImage_front; // front : koreatech badge
     private ImageView mImage_back;  // back : food menu
+    private Animation mSlotAnim;
+
+    @BindView(R.id.activity_main_random_textview)
+    TextView mRandomTextview;
+
 
     private ScaleAnimation sato0 = new ScaleAnimation(1, 0, 1, 1, Animation.RELATIVE_TO_PARENT, 0.5f, Animation.RELATIVE_TO_PARENT, 0.5f);
     private ScaleAnimation sato1 = new ScaleAnimation(0, 1, 1, 1, Animation.RELATIVE_TO_PARENT, 0.5f, Animation.RELATIVE_TO_PARENT, 0.5f);
@@ -21,12 +32,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         initView();
     }
+    @OnClick(R.id.activitiy_main_slotbar_imageview)
+    void onSlotClick(){
+        mRandomTextview.startAnimation(mSlotAnim);
+    }
 
     private void initView() {
         mImage_front = (ImageView)findViewById(R.id.imageview_front_card_00);
         mImage_front.setOnClickListener(this);
         mImage_back = (ImageView)findViewById(R.id.imaggeview_back_card_00);
         mImage_back.setOnClickListener(this);
+
+        ButterKnife.bind(this);
+        mSlotAnim = AnimationUtils.loadAnimation(this, R.anim.slot);
+
+
         showImageFront();
         sato0.setDuration(500);
         sato1.setDuration(500);
