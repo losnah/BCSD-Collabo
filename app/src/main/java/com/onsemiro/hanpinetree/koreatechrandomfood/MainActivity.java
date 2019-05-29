d31201011da2741a2c04700a58913082e7e0c546
dongmin <dongmin960903@gmail.com>
2019-05-30 00:00:05 +0900

    RestaurantActivity추가,MainActivity와RandomActivity분해

--- app/src/main/java/com/onsemiro/hanpinetree/koreatechrandomfood/MainActivity.java
+++ app/src/main/java/com/onsemiro/hanpinetree/koreatechrandomfood/MainActivity.java
@@ -1,16 +1,38 @@
 package com.onsemiro.hanpinetree.koreatechrandomfood;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView mImage_front; // front : koreatech badge
+    private ImageView mImage_back;  // back : food menu
+    private Button randomButton;
+    private ScaleAnimation sato0 = new ScaleAnimation(1, 0, 1, 1, Animation.RELATIVE_TO_PARENT, 0.5f, Animation.RELATIVE_TO_PARENT, 0.5f);
+    private ScaleAnimation sato1 = new ScaleAnimation(0, 1, 1, 1, Animation.RELATIVE_TO_PARENT, 0.5f, Animation.RELATIVE_TO_PARENT, 0.5f);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        randomButton = (Button)findViewById(R.id.random);
+        randomButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),RandomActivity.class);
+                startActivity(intent);
 
-        Toast.makeText(getApplicationContext(),"gg",Toast.LENGTH_LONG).show();
+            }
+        }) ;
+
     }
+
+
 }
